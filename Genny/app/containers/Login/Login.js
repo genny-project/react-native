284b844912f1132d3834ec208c2da0e459f1ad13
Garri Adrian Nablo <garri.almighty@gmail.com>
2017-09-09 18:34:23 +0800

    Add Geolocation to Android and iOS

--- Genny/app/containers/Login/Login.js
+++ Genny/app/containers/Login/Login.js
@@ -11,6 +11,28 @@ class Login extends React.Component {
         headerTintColor: 'white'
     })
 
+    componentDidMount() {
+        this.watchId = navigator.geolocation.watchPosition(
+          (position) => {
+            console.log('lat ' + position.coords.latitude)
+            console.log('lon ' + position.coords.longitude)
+          },
+          (error) => {
+            // handle error
+          },
+          {
+            timeout: 20000,
+            maximumAge: 1000,
+            distanceFilter: 10,
+            enableHighAccuracy: true,
+          }
+        )
+      }
+    
+      componentWillUnmount() {
+        navigator.geolocation.clearWatch(this.watchId)
+      }
+      
     render() {
         return (
             <View style={styles.container}>
