2d10d3cdaad80ca3208fb6f3f6726e854bc820b9
Garri Adrian Nablo <garri.almighty@gmail.com>
2017-09-13 00:12:26 +0800

    Move GPS code to a dedicated container

--- Genny/app/containers/Login/Login.js
+++ Genny/app/containers/Login/Login.js
@@ -5,41 +5,12 @@ import styles from './styles'
 
 class Login extends React.Component {
 
-    static navigationOptions = ({ navigation }) => ({
-        title: 'Login',
-        headerStyle: { backgroundColor: 'orange' },
-        headerTintColor: 'white'
-    })
+  static navigationOptions = ({ navigation }) => ({
+    title: 'Login',
+    headerStyle: { backgroundColor: 'orange' },
+    headerTintColor: 'white'
+  })
 
-    componentDidMount() {
-        this.watchId = navigator.geolocation.watchPosition(
-          (position) => {
-            console.log('lat ' + position.coords.latitude)
-            console.log('lon ' + position.coords.longitude)
-          },
-          (error) => {
-            // handle error
-          },
-          {
-            timeout: 20000,
-            maximumAge: 1000,
-            distanceFilter: 10,
-            enableHighAccuracy: true,
-          }
-        )
-      }
-    
-      componentWillUnmount() {
-        navigator.geolocation.clearWatch(this.watchId)
-      }
-      
-    render() {
-        return (
-            <View style={styles.container}>
-                <Text>Login</Text>
-            </View>
-        )
-    }
 }
 
 export default Login
