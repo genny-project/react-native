8a4da9229587492c0cf01d6761fcccddc026f090
Garri Adrian Nablo <garri.almighty@gmail.com>
2017-09-13 00:17:57 +0800

    Show keycloack login

--- Genny/app/containers/Login/Login.js
+++ Genny/app/containers/Login/Login.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { View, Text } from 'react-native'
+import { WebView } from 'react-native'
 
 import styles from './styles'
 
@@ -40,6 +40,18 @@ class Login extends React.Component {
             </View>
         )
     }
+  render() {
+    const source = { uri: 'https://keycloak.pleasedproperty.com.au/auth/realms/Genny/account' }
+    return (
+      <WebView
+        source={source}
+        style={styles.container}
+        scrollEnabled={false}
+        bounces={false}
+      />
+    )
+  }
+
 }
 
 export default Login
